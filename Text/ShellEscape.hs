36af7c543a96a1aa3b42f83cafb57bed995a25c9
Jason Dusek <jason.dusek@gmail.com>
2010-08-23 17:40:47 +0000

    Should export types, though...

--- Text/ShellEscape.hs
+++ Text/ShellEscape.hs
@@ -1,5 +1,11 @@
 
-module Text.ShellEscape where
+module Text.ShellEscape
+  ( module Text.ShellEscape.Escape
+  , module Text.ShellEscape.Sh
+  , module Text.ShellEscape.Bash
+  ) where
 
 import Text.ShellEscape.Escape
+import Text.ShellEscape.Sh (Sh())
+import Text.ShellEscape.Bash (Bash())
 
