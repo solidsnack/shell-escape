4cc56fe2643e0fa9843d35d68f14468b99846cdf
Jason Dusek <jason.dusek@gmail.com>
2010-08-25 09:36:16 +0000

    Variable number of arguments.

--- test/Echo.hs
+++ test/Echo.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings
   #-}
 
+import System.Environment
 import System.Process (runInteractiveProcess, waitForProcess, ProcessHandle)
 import System.IO (Handle, stderr, stdout, stdin)
 import Data.ByteString (ByteString, pack, hGetContents, hPutStrLn)
 import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString.Char8 as Char8 (pack)
 import Data.Word
 import Control.Monad
 
@@ -29,13 +31,13 @@ prop_echoBash                =  monadicIO $ do
 echoTest escaped             =  do
   b                         <-  echo escaped
   when (b /= unescaped) $ do
-    putStrLn "Echo result differs from unescaped result:"
-    putStrLn "Escaped form:"
-    putStrLn (show $ bytes escaped)
-    putStrLn "Output of echo:"
-    putStrLn (show b)
-    putStrLn "Original:"
-    putStrLn (show unescaped)
+    err "Echo result differs from unescaped result:"
+    err "Escaped form:"
+    err (Char8.pack . ("  "++) . show $ bytes escaped)
+    err "Output of echo:"
+    err (Char8.pack . ("  "++) $ show b)
+    err "Original:"
+    err (Char8.pack . ("  "++) $ show unescaped)
   return (b == unescaped)
  where
   unescaped                  =  unescape escaped
@@ -62,18 +64,28 @@ sh s = runInteractiveProcess "sh" ["-c", s] Nothing Nothing
 bash s = runInteractiveProcess "bash" ["-c", s] Nothing Nothing
 
 instance Arbitrary ByteString where
-  arbitrary                  =  fmap (pack . filter (/= 0x00)) arbitrary
+  arbitrary                  =  fmap pack arbitrary
 
 instance Arbitrary Word8 where
-  arbitrary                  =  fmap fromIntegral (choose ( 0x00 :: Int
+  arbitrary                  =  fmap fromIntegral (choose ( 0x01 :: Int
                                                           , 0xFF :: Int ))
 
 main                         =  do
---hPutStrLn stderr "Testing Bourne Shell escaping."
+  args                      <-  getArgs
+  tests                     <-  case args of
+                                  [arg]       ->  return $ read arg
+                                  []          ->  return 100000
+                                  _           ->  error "Invalid arguments."
+  let msg                    =  "Performing " ++ show tests ++ " tests."
+      qcArgs                 =  Args Nothing tests tests 128
+      qc                     =  quickCheckWith qcArgs
+  err "Tests are random ByteStrings, containing any byte but null."
+--err "Testing Bourne Shell escaping."
+--err (Char8.pack msg)
 --qc prop_echoSh
-  hPutStrLn stderr "Testing Bash escaping."
+  err "Testing Bash escaping."
+  err (Char8.pack msg)
   qc prop_echoBash
- where
-  qcArgs                     =  Args Nothing 100000 100000 128
-  qc                         =  quickCheckWith qcArgs
+
+err                          =  hPutStrLn stderr
 
